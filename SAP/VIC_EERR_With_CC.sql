a2e84fb6bb3e72f7de1f2b1e7e7d65d388e2386e
Ketoprofeno <juliocortesf@gmail.com>
2019-05-13 18:12:36 -0400

    VIC_EERR_With_CC.sql V 1.1

--- SAP/VIC_EERR_With_CC.sql
+++ SAP/VIC_EERR_With_CC.sql
@@ -1,6 +1,6 @@
 DECLARE @ANIO SMALLINT
  
-SET @ANIO = (SELECT A.Year FROM dbo.OACP A WHERE A.Year ='2015')
+SET @ANIO = (SELECT A.Year FROM dbo.OACP A WHERE A.Year ='2018')
  
 SELECT 
     --P.[Year],
@@ -8,25 +8,41 @@ SELECT
 	--P.Name,
 	P.[AcctGroup],
 	P.[Nombre],
-	P.[PrcCode],
-	P.[CCosto],
-	isnull([2015],0) as [2015],
-	isnull([2016],0) as [2016],
-	isnull([2017],0) as [2017],
-	isnull([2018],0) as [2018],
-	isnull([2019],0) as [2019]
-	--isnull([2015],0)+isnull([2016],0)+isnull([2017],0) as [Total]
+	--P.[PrcCode],
+	--P.[CCosto],
+	ISNULL(P.[ZFI.184],0)      [ZFI.184],
+	ISNULL(P.[ZFI.1010],0)     [ZFI.1010],
+	ISNULL(P.[ZFI.2002],0)     [ZFI.2002],
+	ISNULL(P.[ZFI.2077],0)     [ZFI.2077],
+	ISNULL(P.[ZFI.1132],0)     [ZFI.1132],
+	ISNULL(P.[ZFI.6115],0)     [ZFI.6115],
+	ISNULL(P.[ZFI.6130],0)     [ZFI.6130],
+	ISNULL(P.[ZFI.13-1],0)     [ZFI.13-1],
+	ISNULL(P.[ZFI.13-2],0)     [ZFI.13-2],
+	ISNULL(P.[ZFI.13-6],0)     [ZFI.13-6],
+	ISNULL(P.[ZFI.1623],0)     [ZFI.1623],
+	ISNULL(P.[ZFI.17SZ],0)     [ZFI.17SZ],
+	ISNULL(P.[ZFI.1245],0)     [ZFI.1245],
+	ISNULL(P.[ECM.2002],0)     [ECM.2002],
+	ISNULL(P.[G.ADMIN],0)      [G.ADMIN],
+	ISNULL(P.[G.GRAL],0)       [G.GRAL],
+	ISNULL(P.[G.COMER],0)      [G.COMER],
+	ISNULL(P.[G.CONTA],0)      [G.CONTA],
+	ISNULL(P.[MANT],0)         [MANT],
+	ISNULL(P.[INFOR],0)        [INFOR],
+	ISNULL(P.[CORDOVA],0)      [CORDOVA],
+	ISNULL(P.[NULL],0)         [NULL]
+
  
 FROM (
 	SELECT
 	    YEAR(T0.RefDate) AS Year,
 		T0.Account AS Cuenta,
-		--T4.Name,
 		CASE T1.GroupMask
           WHEN 1 THEN '1 Activo'
           WHEN 2 THEN '2 Pasivo'
           WHEN 3 THEN '3 Patrimonio'
-		  -- No van cuentas superiores
+		  --No van cuentas superiores
           WHEN 4 THEN '4 Ingresos Oper.'
           WHEN 5 THEN '5 Costos Oper.'
           WHEN 6 THEN '6 Gastos Oper.'
@@ -36,25 +52,49 @@ FROM (
 		T1.AcctName AS Nombre,
 		T2.PrcCode AS PrcCode,
 		T2.PrcName AS CCosto,
-		MONTH(T0.RefDate)'Month',
 		SUM(T0.Credit-T0.Debit)'CargoAbono' 
  
 	FROM dbo.JDT1 T0
 	INNER JOIN dbo.OACT T1 ON T1.AcctCode=T0.Account
 	LEFT JOIN dbo.OPRC T2 ON T2.PrcCode=T0.ProfitCode
-	--LEFT JOIN [dbo].[@VIC_OCAA] T4 ON T4.Code = T1.U_grupoAnalisis
  
-	WHERE YEAR(T0.RefDate)>= @ANIO AND T1.GroupMask BETWEEN 1 AND 10
+	WHERE YEAR(T0.RefDate)>= 2018 
+	  AND T1.GroupMask BETWEEN 1 AND 10
 	  AND T1.GroupMask NOT IN (1,2,3)
-	  --AND T4.Code = '01'
-	GROUP BY YEAR(T0.RefDate), T0.Account,
-	--T4.Name,
-	T1.GroupMask, T1.AcctName, T2.PrcName,T2.PrcCode ,MONTH(T0.RefDate)
+	GROUP BY 
+	         YEAR(T0.RefDate),
+			 T0.Account,
+	         T1.GroupMask,
+			 T1.AcctName,
+			 T2.PrcName,
+			 T2.PrcCode 
 ) P
  
 PIVOT (
 	SUM(CargoAbono)
-	FOR [Year] IN ([2015],[2016],[2017],[2018],[2019])
-) P
+	FOR PrcCode IN ([ZFI.184],
+					[ZFI.1010],
+					[ZFI.2002],
+					[ZFI.2077],
+					[ZFI.1132],
+					[ZFI.6115],
+					[ZFI.6130],
+					[ZFI.13-1],
+					[ZFI.13-2],
+					[ZFI.13-6],
+					[ZFI.1623],
+					[ZFI.17SZ],
+					[ZFI.1245],
+					[ECM.2002],
+					[G.ADMIN],
+					[G.GRAL],
+					[G.COMER],
+					[G.CONTA],
+					[MANT],
+					[INFOR],
+					[CORDOVA],
+					[NULL]
+					)
+	) P
  
-ORDER BY P.[AcctGroup]--, P.[CCosto]
+ORDER BY P.[AcctGroup]
