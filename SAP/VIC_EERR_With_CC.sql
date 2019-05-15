e27b67b5b078197b89f5a36467d78a289cbea2d7
Ketoprofeno <juliocortesf@gmail.com>
2019-05-15 16:38:03 -0400

    SAP/VIC_EERR_With_CC.sql V 1.2

--- SAP/VIC_EERR_With_CC.sql
+++ SAP/VIC_EERR_With_CC.sql
@@ -3,35 +3,31 @@ DECLARE @ANIO SMALLINT
 SET @ANIO = (SELECT A.Year FROM dbo.OACP A WHERE A.Year ='2018')
  
 SELECT 
-    --P.[Year],
-	P.[Cuenta],
-	--P.Name,
+    P.[Cuenta],
 	P.[AcctGroup],
 	P.[Nombre],
-	--P.[PrcCode],
-	--P.[CCosto],
-	ISNULL(P.[ZFI.184],0)      [ZFI.184],
-	ISNULL(P.[ZFI.1010],0)     [ZFI.1010],
-	ISNULL(P.[ZFI.2002],0)     [ZFI.2002],
-	ISNULL(P.[ZFI.2077],0)     [ZFI.2077],
-	ISNULL(P.[ZFI.1132],0)     [ZFI.1132],
-	ISNULL(P.[ZFI.6115],0)     [ZFI.6115],
-	ISNULL(P.[ZFI.6130],0)     [ZFI.6130],
-	ISNULL(P.[ZFI.13-1],0)     [ZFI.13-1],
-	ISNULL(P.[ZFI.13-2],0)     [ZFI.13-2],
-	ISNULL(P.[ZFI.13-6],0)     [ZFI.13-6],
-	ISNULL(P.[ZFI.1623],0)     [ZFI.1623],
-	ISNULL(P.[ZFI.17SZ],0)     [ZFI.17SZ],
-	ISNULL(P.[ZFI.1245],0)     [ZFI.1245],
-	ISNULL(P.[ECM.2002],0)     [ECM.2002],
-	ISNULL(P.[G.ADMIN],0)      [G.ADMIN],
-	ISNULL(P.[G.GRAL],0)       [G.GRAL],
-	ISNULL(P.[G.COMER],0)      [G.COMER],
-	ISNULL(P.[G.CONTA],0)      [G.CONTA],
-	ISNULL(P.[MANT],0)         [MANT],
-	ISNULL(P.[INFOR],0)        [INFOR],
-	ISNULL(P.[CORDOVA],0)      [CORDOVA],
-	ISNULL(P.[NULL],0)         [NULL]
+	SUM(ISNULL(P.[ZFI.184],0))      [ZFI.184],
+	SUM(ISNULL(P.[ZFI.1010],0))     [ZFI.1010],
+	SUM(ISNULL(P.[ZFI.2002],0))     [ZFI.2002],
+	SUM(ISNULL(P.[ZFI.2077],0))     [ZFI.2077],
+	SUM(ISNULL(P.[ZFI.1132],0))     [ZFI.1132],
+	SUM(ISNULL(P.[ZFI.6115],0))     [ZFI.6115],
+	SUM(ISNULL(P.[ZFI.6130],0))     [ZFI.6130],
+	SUM(ISNULL(P.[ZFI.13-1],0))     [ZFI.13-1],
+	SUM(ISNULL(P.[ZFI.13-2],0))     [ZFI.13-2],
+	SUM(ISNULL(P.[ZFI.13-6],0))     [ZFI.13-6],
+	SUM(ISNULL(P.[ZFI.1623],0))     [ZFI.1623],
+	SUM(ISNULL(P.[ZFI.17SZ],0))     [ZFI.17SZ],
+	SUM(ISNULL(P.[ZFI.1245],0))     [ZFI.1245],
+	SUM(ISNULL(P.[ECM.2002],0))     [ECM.2002],
+	SUM(ISNULL(P.[G.ADMIN],0))      [G.ADMIN],
+	SUM(ISNULL(P.[G.GRAL],0))       [G.GRAL],
+	SUM(ISNULL(P.[G.COMER],0))      [G.COMER],
+	SUM(ISNULL(P.[G.CONTA],0))      [G.CONTA],
+	SUM(ISNULL(P.[MANT],0))         [MANT],
+	SUM(ISNULL(P.[INFOR],0))        [INFOR],
+	SUM(ISNULL(P.[CORDOVA],0))      [CORDOVA],
+	SUM(ISNULL(P.[NULL],0))         [NULL]
 
  
 FROM (
@@ -43,11 +39,11 @@ FROM (
           WHEN 2 THEN '2 Pasivo'
           WHEN 3 THEN '3 Patrimonio'
 		  --No van cuentas superiores
-          WHEN 4 THEN '4 Ingresos Oper.'
-          WHEN 5 THEN '5 Costos Oper.'
-          WHEN 6 THEN '6 Gastos Oper.'
-          WHEN 7 THEN '7 Otros Ingresos'
-          WHEN 8 THEN '8 Otros Gastos'
+          WHEN 4 THEN '4 Ingresos Por Venta'
+          WHEN 5 THEN '5 Costos De Venta'
+          WHEN 6 THEN '6 Gastos DE Admnistración'
+          WHEN 7 THEN '7 Ingresos Financieros'
+          WHEN 8 THEN '8 Gastos Financieros'
         END                                    AS   AcctGroup,
 		T1.AcctName AS Nombre,
 		T2.PrcCode AS PrcCode,
@@ -58,7 +54,7 @@ FROM (
 	INNER JOIN dbo.OACT T1 ON T1.AcctCode=T0.Account
 	LEFT JOIN dbo.OPRC T2 ON T2.PrcCode=T0.ProfitCode
  
-	WHERE YEAR(T0.RefDate)>= 2018 
+	WHERE YEAR(T0.RefDate)= @ANIO 
 	  AND T1.GroupMask BETWEEN 1 AND 10
 	  AND T1.GroupMask NOT IN (1,2,3)
 	GROUP BY 
@@ -96,5 +92,6 @@ PIVOT (
 					[NULL]
 					)
 	) P
- 
+
+GROUP BY P.Cuenta,P.AcctGroup,P.Nombre
 ORDER BY P.[AcctGroup]
